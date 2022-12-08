76f1560145d79502dd1380d0563abcc767bc0192
Rishy Purwar <riteshpuwar2001@gmail.com>
2022-12-08 19:03:19 +0530

    version-1.3#

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Quizaskque from '../components/quizaskque'
-import Quiz from '../components/quiz'
-import Quizpopup from './Quizpopup'
-
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+// import Quizaskque from '../components/quizaskque'
+import Quiz from "../components/quiz";
+import Quizpopup from "./Quizpopup";
 
 const Home: NextPage = () => {
   return (
-    <div >
-    {/* <Quiz/>
+    <div>
+      {/* <Quiz/>
     <Quizaskque/> */}
-    <Quizpopup/>
+      <Quizpopup />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
